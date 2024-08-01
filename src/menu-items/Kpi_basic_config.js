71e1bf6370b99d8d31c82893e505ee21a8286edd
Zerihun Tegenu <zerihuntegenu5@gmail.com>
2024-08-01 05:18:49 -0900

    done merging

--- src/menu-items/Kpi_basic_config.js
+++ src/menu-items/Kpi_basic_config.js
@@ -11,8 +11,7 @@ import {
 } from '@tabler/icons-react';
 
 // constant
-const icons = { IconDashboard ,IconReport,IconShadow,IconActivity,IconSettingsStar,IconFileReport,IconBrandCampaignmonitor};
-
+const icons = { IconDashboard, IconReport, IconShadow, IconActivity, IconSettingsStar, IconFileReport, IconBrandCampaignmonitor };
 
 // ==============================|| Kpi_basic_config  MENU ITEMS ||============================== //
 
@@ -20,85 +19,70 @@ const Kpi_basic_config = {
   id: 'Kpi_basic_config',
   title: 'Planning',
   type: 'group',
-    // requiredRoles: ['Admin'],
+  // requiredRoles: ['Admin'],
   children: [
     {
-        id: 'basic-config',
-        title: 'Pre-Setups',
-        type: 'item',
-        url: '/basic-config/basic-config-creation',
-        icon: icons.IconSettingsStar,
-        
+      id: 'authentication',
+      title: 'Kpi Managment',
+      type: 'collapse',
+      icon: icons.IconFileReport,
+      children: [
+        {
+          id: 'kpi',
+          title: 'Kpi Register',
+          type: 'item',
+          url: '/kpi/kpi-managment'
+          // icon: icons.IconReport,
 
-        // target: true
-      },
-    {
-        id: 'authentication',
-        title: 'Kpi Managment',
-        type: 'collapse',
-        icon: icons.IconFileReport,
-        children: [
-            {
-                id: 'kpi',
-                title: 'Kpi Register',
-                type: 'item',
-                url: '/kpi/kpi-managment',
-                // icon: icons.IconReport,
-                
-        
-                // target: true
-              },
-              {
-                id: 'kpi_tracking',
-                title: 'Kpi Tracking',
-                type: 'item',
-                url: '/kpi-tracking/kpi-track',
-                // icon: icons.IconReport,
-                
-        
-                // target: true
-              },
+          // target: true
+        },
+        {
+          id: 'kpi_tracking',
+          title: 'Kpi Tracking',
+          type: 'item',
+          url: '/kpi-tracking/kpi-track'
+          // icon: icons.IconReport,
 
-        ]
+          // target: true
+        }
+      ]
     },
 
     {
-        id: 'target-settings',
-        title: 'Target Settings',
-        type: 'item',
-        // url: '/basic-config/basic-config-creation',
-        icon: icons.IconReport,
-    
-        // target: true
-      },
-      {
-        id: 'mointoring',
-        title: 'Mointoring',
-        type: 'collapse',
-        icon: icons.IconBrandCampaignmonitor,
-        children: [
-            {
-                id: 'daily-activity',
-                title: 'Daily Activity Settings',
-                type: 'item',
-                // url: '/kpi/kpi-managment',
-                // icon: icons.IconReport,
-                
-        
-                // target: true
-              },
-              {
-                id: 'moitoring-evaluation',
-                title: 'Mointoring & Evaluation',
-                type: 'item',
-                // url: '/kpi/kpi-managment',
-                // icon: icons.IconReport,
-                
-        
-                // target: true
-              },
+      id: 'planning',
+      title: 'Planning',
+      type: 'item',
+      url: '/planning',
+      icon: icons.IconDashboard
 
-        ]
+      // target: true
+    },
+
+    {
+      id: 'mointoring',
+      title: 'Monitoring',
+      type: 'collapse',
+      icon: icons.IconBrandCampaignmonitor,
+      children: [
+        {
+          id: 'daily-activity',
+          title: 'Daily Activity Settings',
+          type: 'item'
+          // url: '/kpi/kpi-managment',
+          // icon: icons.IconReport,
+
+          // target: true
+        },
+        {
+          id: 'moitoring-evaluation',
+          title: 'Mointoring & Evaluation',
+          type: 'item'
+          // url: '/kpi/kpi-managment',
+          // icon: icons.IconReport,
+
+          // target: true
+        }
+      ]
     },
 
     {
